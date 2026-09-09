ce28bebddae47abf0640b87ac303122f4c1029a0
Jahdiel Adrián Tzec Navarrete <jahdieltzec914@gmail.com>
2026-09-08 18:09:21 -0600

    Adding HelloWorld to test Spring Configuration

---
+++ src/main/java/com/tecnm/merida/market_backend_26_3/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.tecnm.merida.market_backend_26_3;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/saludar")
+public class HelloWorldController {
+
+    @GetMapping("/saludo")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
